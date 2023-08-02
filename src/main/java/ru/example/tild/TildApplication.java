a161fc20922698077ee7406f11f2aec187880991
Danil Losev <u4iha.danil@yandex.ru>
2023-08-02 17:51:13 +0300

    Project init.

---
+++ src/main/java/ru/example/tild/TildApplication.java
@@ -0,0 +1,13 @@
+package ru.example.tild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TildApplication.class, args);
+    }
+
+}
